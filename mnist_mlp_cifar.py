2aa25b34f9a154a592f5f9b81db919de10d53f78
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-26 02:38:16 +0300

    batch size

--- mnist_mlp_cifar.py
+++ mnist_mlp_cifar.py
@@ -8,8 +8,8 @@ import keras.regularizers as reg
 from keras.src.layers import Flatten
 
 # batch_size = 128
-batch_size = 256
-# batch_size = 512
+# batch_size = 256
+batch_size = 512
 num_classes = 10
 epochs = 20
 # epochs = 10
