823e1a556db466dbfba1d204539677a8d07c4b40
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-26 01:17:43 +0300

    batch size

--- mnist_mlp_cifar.py
+++ mnist_mlp_cifar.py
@@ -3,14 +3,19 @@ import keras
 from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
+import keras.optimizers as opt
+import keras.regularizers as reg
 
 batch_size = 128
+# batch_size = 256
+# batch_size = 512
 num_classes = 10
 epochs = 20
+# epochs = 10
+# epochs = 100
 
 # the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = cifar10.mnist.load_data()
 
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
@@ -32,15 +37,86 @@ model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(10, activation='softmax'))
 
+# no dropout
+
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dense(10, activation='softmax'))
+
+# l1
+
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l1(0.01)))
+# model.add(Dense(10, activation='softmax'))
+#
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l1(0.1)))
+# model.add(Dense(10, activation='softmax'))
+#
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l1(0.005)))
+# model.add(Dense(10, activation='softmax'))
+
+# l2
+
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.01)))
+# model.add(Dense(10, activation='softmax'))
+#
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.1)))
+# model.add(Dense(10, activation='softmax'))
+#
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.005)))
+# model.add(Dense(10, activation='softmax'))
+
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dropout(0.4))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dropout(0.4))
+# model.add(Dense(10, activation='softmax'))
+#
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dropout(0.8))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dropout(0.8))
+# model.add(Dense(10, activation='softmax'))
+
+# activation functions
+# model.add(Dropout(0.2))
+# model.add(Dense(10, activation='tanh'))
+#
+# model.add(Dropout(0.2))
+# model.add(Dense(10, activation='sigmoid'))
+#
+# model.add(Dropout(0.2))
+# model.add(Dense(10, activation='gelu'))
+
+
 model.summary()
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
+              optimizer=opt.RMSprop(),
               metrics=['accuracy'])
+
+# model.compile(loss='categorical_crossentropy',
+#               optimizer=opt.SGD(),
+#               metrics=['accuracy'])
+
+# model.compile(loss='categorical_crossentropy',
+#               optimizer=opt.Nadam(),
+#               metrics=['accuracy'])
+
+# model.compile(loss='categorical_crossentropy',
+#               optimizer=opt.Adam(),
+#               metrics=['accuracy'])
+
 
 history = model.fit(x_train, y_train,
                     batch_size=batch_size, epochs=epochs,
                     verbose=1, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
+
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
