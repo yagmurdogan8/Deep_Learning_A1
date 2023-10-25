79bb8475db96a2f7908e883974df326fe02ad0d7
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-26 01:13:54 +0300

    batch size

--- mnist_mlp_fashion.py
+++ mnist_mlp_fashion.py
@@ -117,5 +117,6 @@ history = model.fit(x_train, y_train,
                     batch_size=batch_size, epochs=epochs,
                     verbose=1, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
+
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
