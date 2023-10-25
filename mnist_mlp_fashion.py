30c8ab2aba806749e14465444ab11d9f7124d713
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-26 00:40:51 +0300

    reg l2

--- mnist_mlp_fashion.py
+++ mnist_mlp_fashion.py
@@ -53,13 +53,13 @@ model = Sequential()
 # model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.01)))
 # model.add(Dense(10, activation='softmax'))
 
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.1)))
-model.add(Dense(10, activation='softmax'))
-
 # model.add(Dense(512, activation='relu', input_shape=(784,)))
-# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.005)))
+# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.1)))
 # model.add(Dense(10, activation='softmax'))
+
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dense(512, activation='relu', kernel_regularizer=reg.l2(0.005)))
+model.add(Dense(10, activation='softmax'))
 
 # model.add(Dense(512, activation='relu', input_shape=(784,)))
 # model.add(Dropout(0.4))
