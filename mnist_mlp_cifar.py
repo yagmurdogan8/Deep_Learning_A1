6a58b7ec104ca2e0b156397f5859bd4da716a8dc
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-26 01:26:30 +0300

    batch size

--- mnist_mlp_cifar.py
+++ mnist_mlp_cifar.py
@@ -17,8 +17,8 @@ epochs = 20
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
+x_train = x_train.reshape(153600000, 784)
+x_test = x_test.reshape(153600, 784)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
