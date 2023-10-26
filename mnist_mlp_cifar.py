7f6dbeeaf211bb04d105d35ddbd1e251c2175d9c
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-26 03:00:10 +0300

    l1

--- mnist_mlp_cifar.py
+++ mnist_mlp_cifar.py
@@ -39,16 +39,16 @@ model.add(Dense(10, activation='softmax'))
 
 # no dropout
 
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dense(512, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+# model.add(Dense(512, activation='relu', input_shape=(784,)))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dense(10, activation='softmax'))
 
 # l1
 
-# model.add(Dense(512, activation='relu', input_shape=(784,)))
-# model.add(Dense(512, activation='relu', kernel_regularizer=reg.l1(0.01)))
-# model.add(Dense(10, activation='softmax'))
-#
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dense(512, activation='relu', kernel_regularizer=reg.l1(0.01)))
+model.add(Dense(10, activation='softmax'))
+
 # model.add(Dense(512, activation='relu', input_shape=(784,)))
 # model.add(Dense(512, activation='relu', kernel_regularizer=reg.l1(0.1)))
 # model.add(Dense(10, activation='softmax'))
