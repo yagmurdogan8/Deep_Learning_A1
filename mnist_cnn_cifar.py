60354eb3485e0d2c9f0ec849a2202aea945db5a0
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-28 00:35:52 +0300

    nadam

--- mnist_cnn_cifar.py
+++ mnist_cnn_cifar.py
@@ -6,30 +6,31 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
-batch_size = 128
-# batch_size = 256
+# batch_size = 128
+batch_size = 256
 # batch_size = 512
 num_classes = 10
 epochs = 12
 # epochs = 20
 # epochs = 10
 
-# input image dimensions
-img_rows, img_cols = 28, 28
-
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 img_rows, img_cols, img_channels = 32, 32, 3
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, img_channels)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, img_channels)
+input_shape = (img_rows, img_cols, img_channels)
+#
+# if K.image_data_format() == 'channels_first':
+#     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+#     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+#     input_shape = (1, img_rows, img_cols)
+# else:
+#     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+#     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+#     input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
