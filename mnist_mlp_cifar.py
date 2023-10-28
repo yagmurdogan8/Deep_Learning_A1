a911fc85bb9d507113fab281411113e656e9483f
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-29 00:49:48 +0300

    cnn cifar

--- mnist_mlp_cifar.py
+++ mnist_mlp_cifar.py
@@ -122,7 +122,7 @@ from __future__ import print_function
 import keras
 from keras.datasets import cifar10
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Flatten
 import keras.optimizers as opt
 import keras.regularizers as reg
 
@@ -138,8 +138,6 @@ num_classes = 10
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-x_train = x_train.reshape(50000, 784)
-x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
@@ -154,7 +152,8 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Flatten(input_shape=(img_rows, img_cols, img_channels)))
+model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
@@ -185,7 +184,7 @@ for dropout in dropout_values:
                       f"activation={activation_function}", f"optimizer = {optimizer}")
 
                 model = Sequential()
-                model.add(Dense(512, activation=activation_function, input_shape=(784,),
+                model.add(Dense(512, activation=activation_function,
                                 kernel_regularizer=reg.l2(reg_value)))
                 model.add(Dropout(dropout))
                 model.add(Dense(512, activation=activation_function, kernel_regularizer=reg.l2(reg_value)))
