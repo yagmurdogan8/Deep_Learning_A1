3d047916234d3fa3fd3139a6381942c6e6abaa28
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-28 04:05:10 +0300

    cnn cifar

--- mnist_cnn_cifar.py
+++ mnist_cnn_cifar.py
@@ -51,7 +51,7 @@ model.add(Conv2D(32, kernel_size=(3, 3),
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 # model.add(Conv2D(64, (8, 8), activation='relu'))
-model.add(MaxPooling2D(pool_size=(4, 4)))
+# model.add(MaxPooling2D(pool_size=(4, 4)))
 model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
